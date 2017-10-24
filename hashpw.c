b9fa9d94ad571099b232560b28df74fbfb8115f1
William Fu <wfu@college.harvard.edu>
2017-10-24 21:53:04 +0000

    Some updates on writeup for parts 1, 2

--- hashpw.c
+++ hashpw.c
@@ -1,9 +1,54 @@
 /**
  * hashpw.c
- *
- * TODO: thoroughly describe how you reverse-engineered the hash function.
+ * -----------------------
+ * I mostly just looked at the assembly, allocating variables and 
+ * tracking the registers by hand. For a thorough description, the 
+ * comments that I made for myself while going through the assembly
+ * are pasted as a comment below: 
  */
 
+/**
+08048c1b <calculate_pw_hash>:
+push   %ebp
+mov    %esp,%ebp
+sub    $0x10,%esp
+movl   $0x0,-0xc(%ebp)      // Initialize 4 byte variable A
+movl   $0x0,-0x8(%ebp)      // Initialize 4 byte variable B, C
+jmp    8048c59 <calculate_pw_hash+0x3e>
+
+
+mov    -0x8(%ebp),%edx     // move B to %edx
+mov    0x8(%ebp),%eax      // move 1st parameter to eax
+add    %edx,%eax           // add B and 2nd parameter
+movzbl (%eax),%eax         // MOVZBL and MOVSBL is equivalent to 
+movsbl %al,%eax            // getting an index [i] from an array
+mov    %eax,-0x4(%ebp)     // put that value in C
+mov    -0x4(%ebp),%eax.    // put C in eax  
+shl    $0x8,%eax           // bitshift C by 8
+mov    %eax,%edx.          // put (C << 8) in edx
+mov    -0x4(%ebp),%eax.    // put C in eax
+add    %edx,%eax.          // put (C << 8) + c) in eax
+xor    %eax,-0xc(%ebp).    // A ^= (C << 8) + C 
+shll   -0xc(%ebp).         // bitshift A by 1 (A << 1)
+addl   $0x1,-0x8(%ebp).    // increment B
+mov    -0x8(%ebp),%eax
+cmp    0xc(%ebp),%eax.     // if second parameter is less than B
+jl     8048c31 <calculate_pw_hash+0x16>
+cmpl   $0x0,0xc(%ebp).     // jump if 2nd parameter is 0
+jle    8048c73 <calculate_pw_hash+0x58>
+
+mov    0x8(%ebp),%eax
+movzbl (%eax),%eax
+movsbl %al,%eax
+or     %eax,-0xc(%ebp)
+mov    -0xc(%ebp),%eax.   
+xor    $0xfeedface,%eax   // xor A with 0xFEEDFACE
+mov    %eax,-0xc(%ebp)    // put A in the return place  
+mov    -0xc(%ebp),%eax
+
+leave
+ret
+*/
 
 unsigned int hashpw(const char* plaintext, int len) {
 	
